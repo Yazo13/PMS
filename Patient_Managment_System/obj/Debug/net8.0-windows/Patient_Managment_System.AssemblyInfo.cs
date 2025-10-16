1140840384ac373607940735de2ebcbd7d7c6f56
Yazo13 <giorgi.kazishvili@gau.edu.ge>
2025-10-16 17:07:18 +0400

    all done so far

--- Patient_Managment_System/obj/Debug/net8.0-windows/Patient_Managment_System.AssemblyInfo.cs
+++ Patient_Managment_System/obj/Debug/net8.0-windows/Patient_Managment_System.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Patient_Managment_System")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+539808891e0368835b6b92e4c46342162906a715")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c96eb9c474bdb07254e0ffd4608c5459d1c98911")]
 [assembly: System.Reflection.AssemblyProductAttribute("Patient_Managment_System")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Patient_Managment_System")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
